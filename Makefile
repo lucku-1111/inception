65684d09ac1df92e6ec6c6fd84914fee34fb014e
lucku-1111 <lsw6731@naver.com>
2024-07-14 19:15:07 +0900

    wordpress ...ing

--- Makefile
+++ Makefile
@@ -7,22 +7,22 @@ check_mark := $(bold)$(green)✔$(normal)
 red_x := $(bold)$(red)✘$(normal)
 
 up:
-	@sudo mkdir -p ${HOME}/data/mariadb
+	@sudo mkdir -p ${HOME}/data/mariadb ${HOME}/data/wordpress
 
-	@docker-compose -f ./srcs/docker-compose.yml up -d > /dev/null 2>&1 && echo " $(check_mark) Services are up" || (echo "$(red_x) Failed to start services")
+	@docker-compose -f ./srcs/docker-compose.yml up -d
 
 all: up
 
 down:
 	@docker-compose -f ./srcs/docker-compose.yml down
 
-re: down
+re:
 	@docker-compose -f ./srcs/docker-compose.yml up -d --build
 
 clean: down
 	@docker image prune -f > /dev/null 2>&1 && echo " $(check_mark) Removed dangling images" || (echo "$(red_x) Failed to remove dangling images")
-	@docker rmi -f mariadb > /dev/null 2>&1 && echo " $(check_mark) Removed images" || (echo "$(red_x) Failed to remove images")
-	@docker volume rm database > /dev/null 2>&1 && echo " $(check_mark) Removed volumes" || (echo "$(red_x) Failed to remove volumes")
+	@docker rmi -f mariadb wordpress > /dev/null 2>&1 && echo " $(check_mark) Removed images" || (echo "$(red_x) Failed to remove images")
+	@docker volume rm database wordpress > /dev/null 2>&1 && echo " $(check_mark) Removed volumes" || (echo "$(red_x) Failed to remove volumes")
 	@sudo rm -rf ${HOME}/data && echo " $(check_mark) Removed local data" || (echo "$(red_x) Failed to remove local data")
 
 rebuild: clean all
---
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+if cat /etc/php81/php-fpm.d/www.conf | grep -q "user = nobody"; then
+	echo "Configuring PHP-FPM..."
+	sed -i 's/.*user = nobody.*/user = nginx/' /etc/php81/php-fpm.d/www.conf
+	sed -i 's/.*group = nobody.*/group = nginx/' /etc/php81/php-fpm.d/www.conf
+	sed -i 's/.*listen = 127.0.0.1.*/listen = 9000/g' /etc/php81/php-fpm.d/www.conf
+else
+	echo "PHP-FPM is already configured."
+fi
+
+# for i in $(seq 60); do
+# 	if ! mysqladmin -u $MYSQL_USER -p$MYSQL_USER_PASSWORD -h $MYSQL_HOST ping 2> /dev/null; then
+# 		echo "Waiting for MySQL/MariaDB server to be ready... ($i sec)"
+# 		sleep 1
+# 	else
+# 		break
+# 	fi
+# done
+
+# Check if WordPress is already installed
+if ! wp core is-installed --path=/var/www/html/wordpress; then
+	# Create wp-config.php
+	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_USER_PASSWORD --dbhost=$MYSQL_HOST --extra-php <<PHP
+	define( 'WP_CACHE', false );
+PHP
+	# Install WordPress
+	wp core install --url=$WP_URL --title="$WP_TITLE" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --path=/var/www/html/wordpress
+	# Create a new user
+	wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author --path=/var/www/html/wordpress
+	echo "WordPress installation and configuration completed."
+else
+	echo "WordPress is already installed."
+fi
+
+# Start PHP-FPM
+echo "Starting PHP-FPM..."
+php-fpm8.1 -F
